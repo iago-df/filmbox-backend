78ee7b7f37ab69fcad024bc1bf953baccf082a18
dcurrasv25 <dcurrasv25@fpcoruna.afundacion.org>
2026-01-13 09:10:46 +0100

    Add class GetMovieView

--- FilmAPI/rest_api/urls.py
+++ FilmAPI/rest_api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from . import views
+from .views import GetMovieView
 
 urlpatterns = [
-    path('movies/<int:film_id>', views.get_movie, name='get_movie'),
+    path('movies/<int:film_id>', GetMovieView.as_view(), name='get_movie'),
 ]
--- FilmAPI/rest_api/views.py
+++ FilmAPI/rest_api/views.py
@@ -1,25 +1,20 @@
-from django.http import JsonResponse
-from django.views.decorators.http import require_http_methods
-from .models import Film, Category
-from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from .models import Film
 from .serializers import FilmSerializer
 
-# Create your views here.
 
-@api_view(['GET'])
-def get_movie(request, film_id):
-    """Devuelve los datos detallados de una película en formato JSON usando DRF.
+class GetMovieView(APIView):
 
-    Responses:
-    - 200: datos de la película
-    - 404: {"error": "Movie not found"}
-    """
-    try:
-        film = Film.objects.get(pk=film_id)
-    except Film.DoesNotExist:
-        return Response({"error": "Movie not found"}, status=status.HTTP_404_NOT_FOUND)
+    def get(self, request, film_id):
+        try:
+            film = Film.objects.get(pk=film_id)
+        except Film.DoesNotExist:
+            return Response(
+                {"error": "Movie not found"},
+                status=status.HTTP_404_NOT_FOUND
+            )
 
-    serializer = FilmSerializer(film)
-    return Response(serializer.data, status=status.HTTP_200_OK)
+        serializer = FilmSerializer(film)
+        return Response(serializer.data, status=status.HTTP_200_OK)
