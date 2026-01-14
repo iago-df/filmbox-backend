80c063a5608a65bda48b755cd561e5b3012dc344
dcurrasv25 <dcurrasv25@fpcoruna.afundacion.org>
2026-01-14 12:49:06 +0100

    Add rest framework

--- FilmAPI/FilmAPI/settings.py
+++ FilmAPI/FilmAPI/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework'
     'rest_api.apps.RestApiConfig',
     'django.contrib.admin',
     'django.contrib.auth',
